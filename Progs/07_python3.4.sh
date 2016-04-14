729d87468b58f1855156aebca479a6ca55b68504
Felipe Duarte dos Reis <felipe@localhost.localdomain>
2016-04-14 18:07:28 -0300

    Corrigido link do python3.4

--- Progs/07_python3.4.sh
+++ Progs/07_python3.4.sh
@@ -20,7 +20,7 @@ mv python*.rpm $RPM_CONTRIB_DIR
 
 cp $BASE_DIR/Modules/python3.4 /etc/modulefiles/
 
-ln -s /opt/python3.4/python3 /opt/python3.4/python
+ln -s /opt/python3.4/bin/python3 /opt/python3.4/bin/python
 
 #install pip for python3.4
 module load python3.4
@@ -28,7 +28,7 @@ module load python3.4
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 
-ln -s /opt/python3.4/pip3 /opt/python3.4/pip
+ln -s /opt/python3.4/bin/pip3 /opt/python3.4/bin/pip
 
 #installing useful libraries for python 3.4
 pip install numpy 
