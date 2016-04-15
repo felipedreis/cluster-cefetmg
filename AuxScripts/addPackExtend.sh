a8d03f21ae7604ab546870ddcd3e8be6595aab91
juanlopesf <juanlopesf@gmail.com>
2016-04-14 21:06:04 -0300

    Rotina para adicionar pacotes ao extend-compute.xml

--- AuxScripts/addPackExtend.sh
+++ AuxScripts/addPackExtend.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
 
+cd /export/rocks/install/site-profiles/$ROCKS_VER/nodes/
+
+touch extend-compute.xml.tmp
+
+if [ $? -ne 0 ];then
+	echo "Erro ao criar arquivo temporario \"extend-compute.xml.tmp\"" 1>&2
+	exit 1;
+fi
+
+IFS=$'\n'
+while read line;do
+	if [ "$line" == "</pre>" ];then
+		echo -e $line >> extend-compute.xml.tmp;
+		echo -e "<package>$1</package>" >> extend-compute.xml.tmp
+	else
+		echo -e $line >> extend-compute.xml.tmp;
+	fi
+done < extend-compute.xml
+if [ $? -ne 0 ];then
+	echo "Erro ao adicionar pacotes ao \"extend-compute.xml.tmp\"" 1>&2
+	exit 1;
+fi
+
+mv extend-compute.xml extend-compute.xml.bkp
+if [ $? -ne 0 ];then
+	echo "Falha ao realizar backup do arquivo \"extend-compute.xml\"" 1>&2
+	exit 1;
+fi
+
+mv extend-compute.xml.tmp extend-compute.xml
+if [ $? -ne 0 ];then
+	echo "Falha ao renomear arquivo \"extend-compute.xml.tmp\"" 1>&2
+	exit 1;
+fi
