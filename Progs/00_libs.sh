e3f9849e7bd9d4c6a51ad7494fe8b1a9cead5e9d
Felipe Duarte dos Reis <felipe@localhost.localdomain>
2016-04-18 17:10:44 -0300

    Alterado script de instalacao das libs, para instalar libs nos computes

--- Progs/00_libs.sh
+++ Progs/00_libs.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-yum --enablerepo="base, epel" install blas-devel.i686 blas-devel.x86_64 blas.i686 blas.x86_64
-yum --enablerepo="base, epel" install atlas.i686 atlas.x86_64
-yum --enablerepo="base, epel" install lapack.i686 lapack.x86_64 lapack-devel.x86_64 lapack-devel.i686
+mkdir /tmp/libs
+cd /tmp/libs
+
+
+yum --enablerepo="base, epel" --downloadonly --downloaddir=./ install blas-devel.i686 blas-devel.x86_64 blas.i686 blas.x86_64 
+yum --enablerepo="base, epel" --downloadonly --downloaddir=./ install atlas.i686 atlas.x86_64 
+yum --enablerepo="base, epel" --downloadonly --downloaddir=./ install lapack.i686 lapack.x86_64 lapack-devel.x86_64 lapack-devel.i686
+
+rpm -i *.rpm
+
+for pack in $(ls *.rpm);do
+	$BASE_DIR/AuxScripts/addPackExtend.sh $pack
+done
+
+mv * $RPM_CONTRIB_DIR
+
+cd ..
+rm -rf libs
