0363aa8478031d1bb1409e9554e22bf7a1b7bac7
Felipe Duarte dos Reis <felipe@localhost.localdomain>
2016-05-18 17:04:59 -0300

    mesa-libGL installation added in Progs/00_libs.sh

--- Progs/00_libs.sh
+++ Progs/00_libs.sh
@@ -3,10 +3,15 @@
 mkdir /tmp/libs
 cd /tmp/libs
 
-
+# Mathematical libraries
 yum --enablerepo="base, epel" --downloadonly --downloaddir=./ install blas-devel.i686 blas-devel.x86_64 blas.i686 blas.x86_64 
 yum --enablerepo="base, epel" --downloadonly --downloaddir=./ install atlas.i686 atlas.x86_64 
 yum --enablerepo="base, epel" --downloadonly --downloaddir=./ install lapack.i686 lapack.x86_64 lapack-devel.x86_64 lapack-devel.i686
+
+# MESA OpenGL libraries
+# must use yumdownloader as part of them are already installed, yum --downloadonly may fail
+
+yumdownloader --enablerepo="base, epel" mesa-libGL.x86_64 mesa-libGL-devel.x86_64 mesa-libGLU.x86_64 mesa-libGLU-devel.x86_64  mesa-libGLw.x86_64  mesa-libGLw-devel.x86_64  freeglut.x86_64  freeglut-devel.x86_64
 
 rpm -i *.rpm
 
