edc8f4247b78db891f5ce397919b4ec61abb4ca0
root <root@pudim.decom.cefetmg.br>
2016-05-20 19:27:31 -0300

    Corrigido erro no comando sysctl

--- FrontendOnly/08_frontendTuning.sh
+++ FrontendOnly/08_frontendTuning.sh
@@ -6,5 +6,5 @@
 sysctl -w net.ipv4.tcp_max_syn_backlog=4096
 sysctl -w net.core.somaxconn=2048
 
-systcl -w kernel.sched_min_granularity_ns=10000000
-systcl -w kernel.sched_wakeup_granularity_ns=15000000
+sysctl -w kernel.sched_min_granularity_ns=10000000
+sysctl -w kernel.sched_wakeup_granularity_ns=15000000
