3a0a33d7dd070155310bed1cd726360e7b1c62d7
Juan Lopes <juanlopesf@gmail.com>
2016-05-24 20:38:51 -0300

    Correção da instalação do jdk7

--- Progs/01_java.sh
+++ Progs/01_java.sh
@@ -6,11 +6,11 @@ cd /tmp/java
 mkdir -p /opt/java
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn/java/jdk/7u80-b15/jdk-7u80-linux-x64.rpm
-rpm  -i --prefix=/opt/java jdk-7u79-linux-x64.rpm
-rm -f jdk-7u79-linux-x64.rpm
+rpm -i --prefix=/opt/java jdk-7u80-linux-x64.rpm
+rm -f jdk-7u80-linux-x64.rpm
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.rpm
-rpm  -i --prefix=/opt/java jdk-8u91-linux-x64.rpm
+rpm -i --prefix=/opt/java jdk-8u91-linux-x64.rpm
 rm -f jdk-8u91-linux-x64.rpm
 
 yum check
