6ba7b600d468a3350703b5eeab4ab37ec2468ded
Juan Lopes <juanlopesf@gmail.com>
2016-06-17 16:14:15 -0300

    Fix

--- Progs/01_java.sh
+++ Progs/01_java.sh
@@ -5,7 +5,7 @@ cd /tmp/java
 
 mkdir -p /opt/java
 
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.rpm
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.rpm
 if [ ! -f jdk-7u80-linux-x64.rpm ];then
 	echo "Falha ao baixar o arquivo de instalação do JDK7 64 bits"
 	exit 1
@@ -33,11 +33,11 @@ if [ ! -f jdk-8u92-linux-i586.tar.gz ];then
 	exit 1
 fi
 
-mkdir /opt/java/jdk1.7.0_80_32bits 
-mkdir /opt/java/jdk1.8.0_92_32bits
+tar -xzf jdk-7u80-linux-i586.tar.gz
+tar -xzf jdk-8u92-linux-i586.tar.gz
 
-tar -xzf jdk-7u80-linux-i586.tar.gz -C /opt/java/jdk1.7.0_80_32bits
-tar -xzf jdk-8u92-linux-i586.tar.gz -C /opt/java/jdk1.8.0_92_32bits
+mv jdk1.7.0_80 /opt/java/jdk1.7.0_80_32bits
+mv jdk1.8.0_92 /opt/java/jdk1.8.0_92_32bits
 
 rm -f jdk-7u80-linux-x64.rpm
 rm -f jdk-8u92-linux-x64.rpm
