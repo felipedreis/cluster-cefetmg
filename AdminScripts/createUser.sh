164460bb1fe582798f7fbe6a5c3791c5811fd667
felipedreis <secreto335473>
2016-07-20 15:12:04 -0300

    Mais QOS

--- AdminScripts/createUser.sh
+++ AdminScripts/createUser.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-if [ ! $# -eq 3 ] && [ ! $# -eq 1 ];then
-	echo "$0 usuario email data_de_expiracao(aaaa-mm-dd)"
+if [ ! $# -eq 4 ] && [ ! $# -eq 1 ];then
+	echo "$0 usuario email data_de_expiracao(aaaa-mm-dd) account"
 	echo -e "\t\t\tou"
 	echo "$0 lista_de_usuarios"
 	exit
 fi
 
-if [ $# -eq 3 ];then
+if [ $# -eq 4 ];then
+
+	user=$1
+	email=$2
+	expDate=$3
+	account=$4
 	#Adiciona usuario (-e $3 define a data de expiracao da conta) ($1 nome do usuario)
-	useradd -e $3 $1
+	useradd -e $expDate $user
 	userkey=$(mkpasswd)
-	echo ${userkey} | passwd --stdin $1
-	chage -d 0 $1
+	echo ${userkey} | passwd --stdin $user
+	chage -d 0 $user
 
 	#Adiciona usuario ao grupo default
-	usermod -a -G cluster $1
+	usermod -a -G cluster $user
 
 	#Adiciona alias (usuario/email)
-	echo -e "$1:\t$2" >> /etc/aliases
+	echo -e "$user:\t$email" >> /etc/aliases
 
 	#Recarrega os alias
 	newaliases
 	service postfix reload
 
 	#Define a cota. 4GB soft - 5GB hard
-	setquota $1 4194304 5242880 0 0 /state/partition1
+	setquota $user 4194304 5242880 0 0 /state/partition1
 
 	#Copia o sbatch para o home do usuario
-	cp /root/script /export/home/$1
-	chown $1:$1 /export/home/$1/script
+	cp /root/script /export/home/$user
+	chown $user:$user /export/home/$user/script
 
 	#Arquivo de configuracao para o Blast
 	echo "[NCBI]
@@ -37,18 +42,18 @@ if [ $# -eq 3 ];then
 
 	[mpiBLAST]
 	Shared=$BLASTDB
-	Local=$BLASTDB" > /export/$1/.ncbirc
-	chown $1:$1 /export/$1/.ncbirc
+	Local=$BLASTDB" > /export/$user/.ncbirc
+	chown $user:$user /export/$user/.ncbirc
 
 	#Diretório temporário para o t_coffee
-	mkdir -p /export/$1/.t_coffee/tmp
-	chown -R $1:$1 /export/$1/.t_coffee
+	mkdir -p /export/$user/.t_coffee/tmp
+	chown -R $user:$user /export/$user/.t_coffee
 
-	echo -e "Usuario: $1 \nSenha: $userkey" | mailx -s "Usuario_Cluster" $1
+	echo -e "Usuario: $user \nSenha: $userkey" | mailx -s "Usuario_Cluster" $user
 
 	rocks sync users
 
-	sacctmgr add user $1 DefaultAccount=cluster
+	sacctmgr add user $user Account=$account Fairshare=parent
 	rocks sync config
 	scontrol reconfigure
 else
@@ -57,22 +62,23 @@ else
 		user=$(echo $line | cut -d"," -f1)
 		email=$(echo $line | cut -d"," -f2)
 		expDate=$(echo $line | cut -d"," -f3)
+		account=$(echo $line | cut -d"," -f4)
 
-		useradd -e $3 $1
+		useradd -e $expDate $user
 		userkey=$(mkpasswd)
-		echo ${userkey} | passwd --stdin $1
-		chage -d 0 $1
+		echo ${userkey} | passwd --stdin $user
+		chage -d 0 $user
 
-		usermod -a -G cluster $1
+		usermod -a -G cluster $user
 
-		echo -e "$1:\t$2" >> /etc/aliases
+		echo -e "$user:\t$email" >> /etc/aliases
 		newaliases
 		service postfix reload
 
-		setquota $1 4194304 5242880 0 0 /state/partition1
+		setquota $user 4194304 5242880 0 0 /state/partition1
 
-		cp /root/script /export/home/$1
-		chown $1:$1 /export/home/$1/script
+		cp /root/script /export/home/$user
+		chown $user:$user /export/home/$user/script
 
 		#Arquivo de configuracao para o Blast
 		echo "[NCBI]
@@ -80,16 +86,16 @@ else
 
 		[mpiBLAST]
 		Shared=$BLASTDB
-		Local=$BLASTDB" > /export/$1/.ncbirc
-		chown $1:$1 /export/$1/.ncbirc
+		Local=$BLASTDB" > /export/$user/.ncbirc
+		chown $user:$user /export/$user/.ncbirc
 
 		#Diretório temporário para o t_coffee
-		mkdir -p /export/$1/.t_coffee/tmp
-		chown -R $1:$1 /export/$1/.t_coffee
+		mkdir -p /export/$user/.t_coffee/tmp
+		chown -R $user:$user /export/$user/.t_coffee
 
-		echo -e "Usuario: $1 \nSenha: $userkey" | mailx -s "Usuario_Cluster" $1
+		echo -e "Usuario: $user \nSenha: $userkey" | mailx -s "Usuario_Cluster" $user
 
-		sacctmgr add user $1 DefaultAccount=cluster
+		sacctmgr add user $user Account=$account Fairshare=parent
 	done
 
 	rocks sync users
