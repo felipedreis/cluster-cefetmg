05037de6938e6a67a91f17732dd000f72b45e663
felipedreis <secreto335473>
2016-07-20 12:26:22 -0300

    QOS

--- FrontendOnly/07_configSlurm.sh
+++ FrontendOnly/07_configSlurm.sh
@@ -5,23 +5,29 @@
 #munge tuning
 sed -i 's/# DAEMON_ARGS.*/DAEMON_ARGS="--key-file /etc/munge/munge.key --num-threads 10/g' /etc/sysconfig/munge
 
-echo "y" | sacctmgr add account cluster Description="Cluster default account" Organization=cluster
-echo "y" | sacctmgr modify account cluster set MaxSubmitJobs=100 
-
 cp -f $BASE_DIR/Conf/slurm.conf.test /etc/slurm/slurm.conf
+
+echo "y" | sacctmgr add account cefetmg Description="Cluster default account" Organization=cefetmg
+
+echo "y" | sacctmgr add account "ppgmmc posss pesq ict didatico" Description="User group" parent="cefetmg"
+
+echo "y" | sacctmgr modify account cefetmg set MaxSubmitJobs=100 
 
 qoss=`echo "part"{{30,10,2,1}d,{12,6}h}`
 
-for qos in qoss; do
-	sacctmgr add qos $qos
+for qos in $qoss; do
+	echo "y" |  sacctmgr add qos $qos
 done
 
-sacctmgr modify qos part30d set MaxSubmitJobs=1   MaxWall=31-0:0 Priority=1000
-sacctmgr modify qos part10d set MaxSubmitJobs=3   MaxWall=10-0:0 Priority=1000
-sacctmgr modify qos part2d  set MaxSubmitJobs=15  MaxWall=2-0:0  Priority=1000
-sacctmgr modify qos part1d  set MaxSubmitJobs=30  MaxWall=1-0:0  Priority=1000
-sacctmgr modify qos part12h set MaxSubmitJobs=50  MaxWall=12:00  Priority=1000
-sacctmgr modify qos part6h  set MaxSubmitJobs=100 MaxWall=6:00   Priority=1000
+echo "y" | sacctmgr modify qos part30d set MaxSubmitJobs=1   MaxWall=31-0:0 Priority=1000
+echo "y" | sacctmgr modify qos part10d set MaxSubmitJobs=3   MaxWall=10-0:0 Priority=1000
+echo "y" | sacctmgr modify qos part2d  set MaxSubmitJobs=15  MaxWall=2-0:0  Priority=1000
+echo "y" | sacctmgr modify qos part1d  set MaxSubmitJobs=30  MaxWall=1-0:0  Priority=1000
+echo "y" | sacctmgr modify qos part12h set MaxSubmitJobs=50  MaxWall=12:00  Priority=1000
+echo "y" | sacctmgr modify qos part6h  set MaxSubmitJobs=100 MaxWall=6:00   Priority=1000
 
-sacctmgr modify Account set QosLevel+=part30d where Account=cluster
-sacctmgr modify Account set DefaultQos=part30d where Account=cluster
+echo "y" | sacctmgr modify Account set Fairshare=500 QosLevel=part30d,part10d,part2d,part1d,part12h,part6h DefaultQos=part6h where Account=ppgmmc
+echo "y" | sacctmgr modify Account set Fairshare=400 QosLevel=part30d,part10d,part2d,part1d,part12h,part6h DefaultQos=part6h where Account=posss
+echo "y" | sacctmgr modify Account set Fairshare=300 QosLevel=part30d,part10d,part2d,part1d,part12h,part6h DefaultQos=part6h where Account=pesq
+echo "y" | sacctmgr modify Account set Fairshare=200  QosLevel=part10d,part2d,part1d,part12h,part6h DefaultQos=part6h where Account=ict
+echo "y" | sacctmgr modify Account set Fairshare=100 QosLevel=part6h DefaultQos=part6h where Account=didatico
